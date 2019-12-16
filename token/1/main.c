9a0afe859329da8d45df11367ef090ae7ff195c2
sacani <antoniofernandezcruz@hotmail.com>
2019-12-16 22:05:50 +0100

    201912162205

--- token/1/main.c
+++ token/1/main.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
 #include<string.h>
+#include<stddef.h>
 int main(int argc,char**argv){
-char c[1024
+char*t;
+char*c;
+c=strdup(argv[1]);
+t=strsep(&c," ");
+do{
+fprintf(stdout,"%s\n",t);
+t=strsep(&c," ");
+}while(t!=NULL);
+return 0;
 }
