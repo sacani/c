9028b5595652036494ae54938bd4cbc2cbc0f8f5
sacani <antoniofernandezcruz@hotmail.com>
2020-01-03 15:39:31 +0100

    202001031539

--- token/4/help.c
+++ token/4/help.c
@@ -2,6 +2,6 @@
 int help(){
 fprintf(stdout,"quit: exit\n");
 fprintf(stdout,"help: show this help\n");
-fprintf(stdout,"token sentence:show tokens of sentences\n");
+fprintf(stdout,"token sentence:show tokens of sentence\n");
 return 0;
 }
